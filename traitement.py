99dca27d21e82bc42d9419a6021fec320a17861f
Alexis Delorme <alexis.delorme@outlook.com>
2020-06-05 17:26:31 +0200

    Commencer traitement de données

---
+++ traitement.py
@@ -0,0 +1,114 @@
+from zipfile import ZipFile
+import json
+import re
+
+
+def get_zip_info(country,continent):
+    with ZipFile('{}.zip'.format(continent),'r') as z:
+        # infobox du pays
+        return json.loads(z.read('{}.json'.format(country)))
+
+Barb = get_zip_info('Barbados','north_america')
+
+capital = Barb['capital']
+print('Chaîne brute : {}'.format(capital))
+
+# Analyse de la chaîne brute en la comparant à [[*]], et en mémorisant le contenu des crochets
+m = re.match("\[\[(\w+)\]\]", capital)
+
+capital = m.group(1)
+print('Capitale : {}'.format(capital))
+#
+# Récupération des coordonnées de la capitale depuis l'infobox d'un pays
+#
+
+#
+# Conversion d'une chaîne de caractères décrivant une position géographique
+# en coordonnées numériques latitude et longitude
+#
+def cv_coords(str_coords):
+    # on découpe au niveau des "|" 
+    c = str_coords.split('|')
+
+    # on extrait la latitude en tenant compte des divers formats
+    lat = float(c.pop(0))
+    if (c[0] == 'N'):
+        c.pop(0)
+    elif ( c[0] == 'S' ):
+        lat = -lat
+        c.pop(0)
+    elif ( len(c) > 1 and c[1] == 'N' ):
+        lat += float(c.pop(0))/60
+        c.pop(0)
+    elif ( len(c) > 1 and c[1] == 'S' ):
+        lat += float(c.pop(0))/60
+        lat = -lat
+        c.pop(0)
+    elif ( len(c) > 2 and c[2] == 'N' ):
+        lat += float(c.pop(0))/60
+        lat += float(c.pop(0))/3600
+        c.pop(0)
+    elif ( len(c) > 2 and c[2] == 'S' ):
+        lat += float(c.pop(0))/60
+        lat += float(c.pop(0))/3600
+        lat = -lat
+        c.pop(0)
+
+    # on fait de même avec la longitude
+    lon = float(c.pop(0))
+    if (c[0] == 'W'):
+        lon = -lon
+        c.pop(0)
+    elif ( c[0] == 'E' ):
+        c.pop(0)
+    elif ( len(c) > 1 and c[1] == 'W' ):
+        lon += float(c.pop(0))/60
+        lon = -lon
+        c.pop(0)
+    elif ( len(c) > 1 and c[1] == 'E' ):
+        lon += float(c.pop(0))/60
+        c.pop(0)
+    elif ( len(c) > 2 and c[2] == 'W' ):
+        lon += float(c.pop(0))/60
+        lon += float(c.pop(0))/3600
+        lon = -lon
+        c.pop(0)
+    elif ( len(c) > 2 and c[2] == 'E' ):
+        lon += float(c.pop(0))/60
+        lon += float(c.pop(0))/3600
+        c.pop(0)
+    
+    # on renvoie un dictionnaire avec les deux valeurs
+    return {'lat':lat, 'lon':lon }
+
+
+def get_coords(wp_info):
+
+    # S'il existe des coordonnées dans l'infobox du pays
+    # (cas le plus courant)
+    if 'coordinates' in wp_info:
+
+        # (?i) - ignorecase - matche en majuscules ou en minuscules
+        # ça commence par "{{coord" et se poursuit avec zéro ou plusieurs
+        #   espaces suivis par une barre "|"
+        # après ce motif, on mémorise la chaîne la plus longue possible
+        #   ne contenant pas de },
+        # jusqu'à la première occurence de "}}"
+        m = re.match('(?i).*{{coord\s*\|([^}]*)}}', wp_info['coordinates'])
+
+        # l'expression régulière ne colle pas, on affiche la chaîne analysée pour nous aider
+        # mais c'est un aveu d'échec, on ne doit jamais se retrouver ici
+        if m == None :
+            print(' Could not parse coordinates info {}'.format(wp_info['coordinates']))
+            return None
+
+        # cf. https://en.wikipedia.org/wiki/Template:Coord#Examples
+        # on a récupère une chaîne comme :
+        # 57|18|22|N|4|27|32|W|display=title
+        # 44.112|N|87.913|W|display=title
+        # 44.112|-87.913|display=title
+        str_coords = m.group(1)
+
+        # on convertit en numérique et on renvoie
+        if str_coords[0:1] in '0123456789':
+            return cv_coords(str_coords)
